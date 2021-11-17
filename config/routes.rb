01070d1057141d4822bb0cbf541b262463838db4
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-11-17 20:17:03 +0100

    Revert "Feature/redux store"

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-      resources :greetings, only: [:index]
-    end
-  end
-
-  get '*page', to: 'static#index', constraints: ->(req) do
-    !req.xhr? && req.format.html?
-  end
-
-  root 'static#index'
 end
